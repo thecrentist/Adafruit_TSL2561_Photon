70941ac0320c8f363d3ca87f9f0374c63d36d993
Aaron <thecrentist00@gmail.com>
2016-02-28 00:01:32 -0800

    Update Adafruit_TSL2561_Photon.h

--- firmware/Adafruit_TSL2561_Photon.h
+++ firmware/Adafruit_TSL2561_Photon.h
@@ -2,14 +2,10 @@
 /*! 
     @file     Adafruit_TSL2561.h
     @author   K. Townsend (Adafruit Industries)
-
     @section LICENSE
-
     Software License Agreement (BSD License)
-
     Copyright (c) 2013, Adafruit Industries
     All rights reserved.
-
     Redistribution and use in source and binary forms, with or without
     modification, are permitted provided that the following conditions are met:
     1. Redistributions of source code must retain the above copyright
@@ -20,7 +16,6 @@
     3. Neither the name of the copyright holders nor the
     names of its contributors may be used to endorse or promote products
     derived from this software without specific prior written permission.
-
     THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ''AS IS'' AND ANY
     EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
     WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -36,7 +31,20 @@
 #ifndef _TSL2561_H_
 #define _TSL2561_H_
 
-#include "Adafruit_Sensor.h"
+#if ARDUINO >= 100
+ #include <Arduino.h>
+#else
+ #include <WProgram.h>
+#endif
+#include <Adafruit_Sensor.h>
+
+#ifdef __AVR_ATtiny85__
+  #include "TinyWireM.h"
+  #define Wire TinyWireM
+#else
+  #include <Wire.h>
+#endif
+
 
 #define TSL2561_VISIBLE 2                   // channel 0 - channel 1
 #define TSL2561_INFRARED 1                  // channel 1
@@ -132,65 +140,65 @@
 
 enum
 {
-    TSL2561_REGISTER_CONTROL          = 0x00,
-    TSL2561_REGISTER_TIMING           = 0x01,
-    TSL2561_REGISTER_THRESHHOLDL_LOW  = 0x02,
-    TSL2561_REGISTER_THRESHHOLDL_HIGH = 0x03,
-    TSL2561_REGISTER_THRESHHOLDH_LOW  = 0x04,
-    TSL2561_REGISTER_THRESHHOLDH_HIGH = 0x05,
-    TSL2561_REGISTER_INTERRUPT        = 0x06,
-    TSL2561_REGISTER_CRC              = 0x08,
-    TSL2561_REGISTER_ID               = 0x0A,
-    TSL2561_REGISTER_CHAN0_LOW        = 0x0C,
-    TSL2561_REGISTER_CHAN0_HIGH       = 0x0D,
-    TSL2561_REGISTER_CHAN1_LOW        = 0x0E,
-    TSL2561_REGISTER_CHAN1_HIGH       = 0x0F
+  TSL2561_REGISTER_CONTROL          = 0x00,
+  TSL2561_REGISTER_TIMING           = 0x01,
+  TSL2561_REGISTER_THRESHHOLDL_LOW  = 0x02,
+  TSL2561_REGISTER_THRESHHOLDL_HIGH = 0x03,
+  TSL2561_REGISTER_THRESHHOLDH_LOW  = 0x04,
+  TSL2561_REGISTER_THRESHHOLDH_HIGH = 0x05,
+  TSL2561_REGISTER_INTERRUPT        = 0x06,
+  TSL2561_REGISTER_CRC              = 0x08,
+  TSL2561_REGISTER_ID               = 0x0A,
+  TSL2561_REGISTER_CHAN0_LOW        = 0x0C,
+  TSL2561_REGISTER_CHAN0_HIGH       = 0x0D,
+  TSL2561_REGISTER_CHAN1_LOW        = 0x0E,
+  TSL2561_REGISTER_CHAN1_HIGH       = 0x0F
 };
 
 typedef enum
 {
-    TSL2561_INTEGRATIONTIME_13MS      = 0x00,    // 13.7ms
-    TSL2561_INTEGRATIONTIME_101MS     = 0x01,    // 101ms
-    TSL2561_INTEGRATIONTIME_402MS     = 0x02     // 402ms
+  TSL2561_INTEGRATIONTIME_13MS      = 0x00,    // 13.7ms
+  TSL2561_INTEGRATIONTIME_101MS     = 0x01,    // 101ms
+  TSL2561_INTEGRATIONTIME_402MS     = 0x02     // 402ms
 }
 tsl2561IntegrationTime_t;
 
 typedef enum
 {
-    TSL2561_GAIN_1X                   = 0x00,    // No gain
-    TSL2561_GAIN_16X                  = 0x10,    // 16x gain
+  TSL2561_GAIN_1X                   = 0x00,    // No gain
+  TSL2561_GAIN_16X                  = 0x10,    // 16x gain
 }
 tsl2561Gain_t;
 
 class Adafruit_TSL2561_Unified : public Adafruit_Sensor {
-    public:
-        Adafruit_TSL2561_Unified(uint8_t addr, int32_t sensorID = -1);
-        boolean begin(void);
-        
-        /* TSL2561 Functions */
-        void enableAutoRange(bool enable);
-        void setIntegrationTime(tsl2561IntegrationTime_t time);
-        void setGain(tsl2561Gain_t gain);
-        void getLuminosity (uint16_t *broadband, uint16_t *ir);
-        uint32_t calculateLux(uint16_t broadband, uint16_t ir);
-        
-        /* Unified Sensor API Functions */  
-        bool getEvent(sensors_event_t*);
-        void getSensor(sensor_t*);
-    
-    private:
-        int8_t _addr;
-        boolean _tsl2561Initialised;
-        boolean _tsl2561AutoGain;
-        tsl2561IntegrationTime_t _tsl2561IntegrationTime;
-        tsl2561Gain_t _tsl2561Gain;
-        int32_t _tsl2561SensorID;
-        
-        void     enable (void);
-        void     disable (void);
-        void     write8 (uint8_t reg, uint32_t value);
-        uint8_t  read8 (uint8_t reg);
-        uint16_t read16 (uint8_t reg);
-        void     getData (uint16_t *broadband, uint16_t *ir);
+ public:
+  Adafruit_TSL2561_Unified(uint8_t addr, int32_t sensorID = -1);
+  boolean begin(void);
+  
+  /* TSL2561 Functions */
+  void enableAutoRange(bool enable);
+  void setIntegrationTime(tsl2561IntegrationTime_t time);
+  void setGain(tsl2561Gain_t gain);
+  void getLuminosity (uint16_t *broadband, uint16_t *ir);
+  uint32_t calculateLux(uint16_t broadband, uint16_t ir);
+  
+  /* Unified Sensor API Functions */  
+  bool getEvent(sensors_event_t*);
+  void getSensor(sensor_t*);
+
+ private:
+  int8_t _addr;
+  boolean _tsl2561Initialised;
+  boolean _tsl2561AutoGain;
+  tsl2561IntegrationTime_t _tsl2561IntegrationTime;
+  tsl2561Gain_t _tsl2561Gain;
+  int32_t _tsl2561SensorID;
+  
+  void     enable (void);
+  void     disable (void);
+  void     write8 (uint8_t reg, uint32_t value);
+  uint8_t  read8 (uint8_t reg);
+  uint16_t read16 (uint8_t reg);
+  void     getData (uint16_t *broadband, uint16_t *ir);
 };
 #endif
